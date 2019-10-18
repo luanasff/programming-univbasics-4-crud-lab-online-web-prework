935835319b78baa16e4d349e4ffd5fa9bd1d4e45
Luana Faria <luanasfff@gmail.com>
2019-10-18 17:09:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,12 +35,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    retrieve_first_element_from_array ["wow","array"]
+    retrieve_first_element_from_array= ["wow","array"]
     retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    retrieve_last_element_from_array=["am","wow","array"]
+    retrieve_last_element_from_array
 end
 
 def update_element_from_index(array, index_number, element)
