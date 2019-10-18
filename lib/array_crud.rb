953ac23a5b42a0313656cadb72627035a321538a
Luana Faria <luanasfff@gmail.com>
2019-10-18 17:02:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["array","index_number","am"]
-  retrieve_element_from_index[1]
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
