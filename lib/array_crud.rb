d0661d74394fd1dacd02bdaf880fe8424fcd7a8a
Luana Faria <luanasfff@gmail.com>
2019-10-18 17:13:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -45,5 +45,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+  update_element_from_index= ["array","index","wow"]
+  update_element_from_index
 
 end
