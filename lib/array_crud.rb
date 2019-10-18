f1a46c5f28555c97ba2419f85a1b772a2f26812f
Luana Faria <luanasfff@gmail.com>
2019-10-18 17:16:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   remove_element_from_start_of_array = "wow"
+   remove_element_from_start_of_array = ["wow","wow2"]
    remove_element_from_start_of_array.shift
    
 end
