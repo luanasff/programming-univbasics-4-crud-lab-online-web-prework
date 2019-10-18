61510257f51e0fc2716e11c50523f74ecd9eab45
Luana Faria <luanasfff@gmail.com>
2019-10-18 17:00:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_dogs = ["Toddy", "Vader","Obi","Kylo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array = ["array", "element"]
+  add_element_to_end_of_array << "arrays!"
+   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array = ["array","element"]
+  add_element_to_start_of_array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+   remove_element_from_end_of_array ="array!"
+   remove_element_from_end_of_array.pop
+   
 end
 
 def remove_element_from_start_of_array(array)
-  
+   remove_element_from_start_of_array = "wow"
+   remove_element_from_start_of_array.unshift
+   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index = ["array","index_number"]
+  retrieve_element_from_index[1]
 end
 
 def retrieve_first_element_from_array(array)
